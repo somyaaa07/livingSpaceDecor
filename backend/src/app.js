a89505c340e51bac489d1e417d16e8d41c791410
somya singh <somya.21b0131037@abes.ac.in>
2026-08-06 12:29:44 +0530

    changes

--- backend/src/app.js
+++ backend/src/app.js
@@ -19,8 +19,8 @@ const __dirname = path.dirname(__filename);
 const app = express();
 
 app.use(cors({
-    // origin: 'http://localhost:3000' || "*"
-        origin: 'https://livingspacedecor.in' || "*"
+    origin: 'http://localhost:3000' || "*"
+        // origin: 'https://livingspacedecor.in' || "*"
 
 }));
 
