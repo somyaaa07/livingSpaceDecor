92f0b7e180b258d7f2c7fb842b95f6ba49f9aff3
Ankita Mishra <ankitamishra8763@gmail.com>
2026-08-06 17:12:49 +0530

    update

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -1,43 +1,7 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   images: {
-//     remotePatterns: [
-//       {
-//         protocol: "http",
-//         hostname: "localhost",
-//         port: "5000",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "api.livingspacedecor.in",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.unsplash.com",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "randomuser.me",
-//       },
-//       {
-//         protocol: "https",
-//         hostname: "images.pexels.com",
-//       },
-//     ],
-//   },
-// };
-
-// export default nextConfig;
-
-import bundleAnalyzer from "@next/bundle-analyzer";
-
-const withBundleAnalyzer = bundleAnalyzer({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
+    
     remotePatterns: [
       {
         protocol: "http",
@@ -64,4 +28,5 @@ const nextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+export default nextConfig;
+
